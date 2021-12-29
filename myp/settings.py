cdb0ff467f846b836a587af15f6142819dcd0e12
shubham <shubham.jadhav@ufaber.com>
2021-12-29 15:43:57 +0530

    some settings and changes are done

--- myp/settings.py
+++ myp/settings.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = 'myp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "portfolio",
+        'HOST': "database.czv0fhonw5ck.us-east-2.rds.amazonaws.com",
+        "USER":"admin",
+        "PASSWORD":"redminote3kenzo",
+        "PORT":'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
