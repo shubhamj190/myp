ee58854ec5548424fb47f4ee218b9dcd2081da0d
shubham <shubham.jadhav@ufaber.com>
2021-12-30 23:33:47 +0530

    some changes for templates

--- myp/settings.py
+++ myp/settings.py
@@ -79,25 +79,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myp.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'portfolio',
-        'USER': 'admin',
-        'PASSWORD': 'redminote3kenzo',
-        'HOST': 'portfolio-db.crutcevhgybz.ap-south-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
@@ -168,3 +157,4 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+REPLY_TO_EMAIL = "shubham <shubhamjadhav190@gmail.com>"
